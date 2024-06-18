f1d61831c936f69495c0352caf5c089c57af478d
User <User@WIN-OUPLEQ260DI>
2024-06-18 22:33:34 +0800

    Добавьте файлы проекта.

---
+++ TradersDiary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TradersDiary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
